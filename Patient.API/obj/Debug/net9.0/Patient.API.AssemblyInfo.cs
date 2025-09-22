827d8d6b600dc234ffdedff1a1a6697033371c72
Icanebal <arthurlouismarteau@gmail.com>
2025-09-22 17:41:45 +0200

    init2

--- Patient.API/obj/Debug/net9.0/Patient.API.AssemblyInfo.cs
+++ Patient.API/obj/Debug/net9.0/Patient.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Patient.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9db50fb4f4d0988c1b9fad0f2672073bd6c73d98")]
 [assembly: System.Reflection.AssemblyProductAttribute("Patient.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Patient.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
