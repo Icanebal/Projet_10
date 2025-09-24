1c5b29397f2bf1f21398f8e15ae0644b69f4664b
Icanebal <arthurlouismarteau@gmail.com>
2025-09-24 17:53:24 +0200

    CRUD Patients.API Ok

---
+++ Patients.API/Data/PatientDbContext.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Patients.API.Models.Entities;
+
+namespace Patients.API.Data
+{
+    public class PatientDbContext : DbContext
+    {
+        public PatientDbContext(DbContextOptions<PatientDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Patient>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired();
+
+                entity.Property(e => e.LastName)
+                    .IsRequired();
+
+                entity.Property(e => e.BirthDate)
+                    .IsRequired();
+
+                entity.Property(e => e.Gender)
+                    .IsRequired();
+            });
+
+            SeedTestData(modelBuilder);
+        }
+
+        private static void SeedTestData(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Patient>().HasData(
+                new Patient
+                {
+                    Id = 1,
+                    FirstName = "Test",
+                    LastName = "TestNone",
+                    BirthDate = new DateTime(1966, 12, 31),
+                    Gender = "F",
+                    Address = "1 Brookside St",
+                    Phone = "100-222-3333",
+                },
+                new Patient
+                {
+                    Id = 2,
+                    FirstName = "Test",
+                    LastName = "TestBorderline",
+                    BirthDate = new DateTime(1945, 6, 24),
+                    Gender = "M",
+                    Address = "2 High St",
+                    Phone = "200-333-4444",
+                },
+                new Patient
+                {
+                    Id = 3,
+                    FirstName = "Test",
+                    LastName = "TestInDanger",
+                    BirthDate = new DateTime(2004, 6, 18),
+                    Gender = "M",
+                    Address = "3 Club Road",
+                    Phone = "300-444-5555",
+                },
+                new Patient
+                {
+                    Id = 4,
+                    FirstName = "Test",
+                    LastName = "TestEarlyOnset",
+                    BirthDate = new DateTime(2002, 6, 28),
+                    Gender = "F",
+                    Address = "4 Valley Dr",
+                    Phone = "400-555-6666",
+                }
+            );
+        }
+    }
+}
---
+++ Patients.API/Utilities/Mapping.cs
@@ -0,0 +1,55 @@
+using Patients.API.Models.DTOs;
+using Patients.API.Models.Entities;
+
+namespace Patients.API.Utilities
+{
+    // Classe statique pour centraliser tous les mappings entre entités et DTOs
+    public static class Mapping
+    {
+        // Mapping manuel Entity vers DTO pour les réponses API
+        public static PatientDto MapToDto(Patient patient)
+        {
+            return new PatientDto
+            {
+                Id = patient.Id,
+                FirstName = patient.FirstName,
+                LastName = patient.LastName,
+                BirthDate = patient.BirthDate,
+                Gender = patient.Gender,
+                Address = patient.Address,
+                Phone = patient.Phone
+            };
+        }
+
+        // Mapping manuel CreateDTO vers Entity pour la création
+        public static Patient MapToEntity(CreatePatientDto createDto)
+        {
+            return new Patient
+            {
+                FirstName = createDto.FirstName,
+                LastName = createDto.LastName,
+                BirthDate = createDto.BirthDate,
+                Gender = createDto.Gender,
+                Address = createDto.Address,
+                Phone = createDto.Phone
+            };
+        }
+
+        // Mapping pour mettre à jour une entité existante avec les données du DTO
+        public static void MapUpdateToEntity(CreatePatientDto updateDto, Patient existingPatient)
+        {
+            existingPatient.FirstName = updateDto.FirstName;
+            existingPatient.LastName = updateDto.LastName;
+            existingPatient.BirthDate = updateDto.BirthDate;
+            existingPatient.Gender = updateDto.Gender;
+            existingPatient.Address = updateDto.Address;
+            existingPatient.Phone = updateDto.Phone;
+        }
+
+        // Extension pour mapper une collection d'entités vers DTOs
+        public static IEnumerable<PatientDto> MapToDtoCollection(IEnumerable<Patient> patients)
+        {
+            return patients.Select(MapToDto);
+        }
+    }
+}
